79678ec896020aaa530826c2e1ab3ddb29c45658
tanmaygoyal13 <goyaltanmay.13@gmail.com>
2022-12-07 16:02:41 +0530

    Add files via upload

---
+++ Dice/index.js
@@ -0,0 +1,13 @@
+var num1 = Math.floor(Math.random() * 6) + 1;
+document.querySelector(".img1").setAttribute("src", "images/dice" + num1 + ".png")
+var num2 = Math.floor(Math.random() * 6) + 1;
+document.querySelector(".img2").setAttribute("src", "images/dice" + num2 + ".png")
+if(num1 > num2){
+    document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
+}
+else if(num2 > num1){
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+}
+else{
+    document.querySelector("h1").innerHTML = "Draw!";
+}
